e3c70e22cf64ca868fd0e634ccbb3c26b16194f3
Veronica <72387578+Yun-Veronica@users.noreply.github.com>
2024-11-30 12:50:01 +0500

    Create script.js to module 4 solutions

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var names = ["John", "Jake", "Alice", "Michael", "Jennifer", "Bob"];
+
+function sayHelloOrGoodbye(names) {
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+            console.log("Goodbye " + names[i]); 
+        } else {
+            console.log("Hello " + names[i]); 
+        }
+    }
+}
+
+sayHelloOrGoodbye(names);
